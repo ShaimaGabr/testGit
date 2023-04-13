cc8bd09623c5f6f0038a02de89588abaa7868ea9
ShaimaGabr <89024214+ShaimaGabr@users.noreply.github.com>
2023-04-13 18:31:27 +0200

    Otp PinView

--- app/src/main/java/com/example/myapplicationgit/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationgit/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.myapplicationgit
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.chaos.view.PinView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var pinView: PinView
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pinView=findViewById(R.id.firstPinView)
+        button=findViewById(R.id.button)
+        pinView.requestFocus()
+        val inputMethodManager:InputMethodManager= getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,InputMethodManager.HIDE_IMPLICIT_ONLY)
+        pinView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+               if(p0.toString().length==4){
+                 //  button.visibility=View.VISIBLE
+                   button.isEnabled=true
+                   Toast.makeText(this@MainActivity,"work",Toast.LENGTH_SHORT).show()
+               }else{
+                   button.isEnabled=false
+               }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
     }
 }
